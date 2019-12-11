8268203b2882e6f8da7913dd16bb5ad796811504
Konstantin Budzko <kostik822@gmail.com>
2019-12-11 14:51:48 +0300

    Add main task Java Class

--- src/main/java/budzko/MainTaskJClass/Book.java
+++ src/main/java/budzko/MainTaskJClass/Book.java
@@ -1,4 +1,135 @@
 package budzko.MainTaskJClass;
 
 public class Book {
+    private int id;
+    private String nameOfBook;
+    private String author;
+    private String publishing;
+    private int publicationDate;
+    private int numberOfPages;
+    private double price;
+    private String typeOfBinding;
+
+    public Book(int id, String nameOfBook, String author, String publishing, int publicationDate, int numberOfPages, double price, String typeOfBinding) {
+        this.id = id;
+        this.nameOfBook = nameOfBook;
+        this.author = author;
+        this.publishing = publishing;
+        this.publicationDate = publicationDate;
+        this.numberOfPages = numberOfPages;
+        this.price = price;
+        this.typeOfBinding = typeOfBinding;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishing() {
+        return publishing;
+    }
+
+    public void setPublishing(String publishing) {
+        this.publishing = publishing;
+    }
+
+    public int getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(int publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTypeOfBinding() {
+        return typeOfBinding;
+    }
+
+    public void setTypeOfBinding(String typeOfBinding) {
+        this.typeOfBinding = typeOfBinding;
+    }
+
+    public void showAuthor() {
+        if ("Роман Злотников ".equals(this.getAuthor())) {
+            System.out.println("ID: " + getId());
+            System.out.println("Name of book: " + getNameOfBook());
+            System.out.println("Publishing: " + getPublishing());
+            System.out.println("Publication Date: " + getPublicationDate());
+            System.out.println("Number of pages: " + getNumberOfPages());
+            System.out.println("Price: " + getPrice());
+            System.out.println("Type of binding: " + getTypeOfBinding() + "\n");
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "ID: " + getId() + "\t" + "Name: " + getNameOfBook() + "\t" + "Author: " + getAuthor() + "\t" + "Publishing: " + getPublishing() + "\t"
+                + "Publishing date: " + getPublicationDate() + "\t" + "Number of pages: " + getNumberOfPages() + "\t" + "price: " +
+                getPrice() + "\t" + "type of binding: " + getTypeOfBinding();
+        return s;
+    }
+
+    public void showYear() {
+        if ((this.getPublicationDate()) > 2015) {
+            System.out.println("ID: " + getId());
+            System.out.println("Name of book: " + getNameOfBook());
+            System.out.println("Author: " + getAuthor());
+            System.out.println("Publishing: " + getPublishing());
+            System.out.println("Publication Date: " + getPublicationDate());
+            System.out.println("Number of pages: " + getNumberOfPages());
+            System.out.println("Price: " + getPrice());
+            System.out.println("Type of binding: " + getTypeOfBinding() + "\n");
+
+        }
+    }
+
+    public void showPublishing() {
+        if ("АСТ " == this.getPublishing()) {
+            System.out.println("ID: " + getId());
+            System.out.println("Name of book: " + getNameOfBook());
+            System.out.println("Author: " + getAuthor());
+            System.out.println("Publication Date: " + getPublicationDate());
+            System.out.println("Number of pages: " + getNumberOfPages());
+            System.out.println("Price: " + getPrice());
+            System.out.println("Type of binding: " + getTypeOfBinding() + "\n");
+
+        }
+    }
 }
---
+++ src/main/java/budzko/MainTaskJClass/Main.java
@@ -0,0 +1,31 @@
+package budzko.MainTaskJClass;
+
+
+public class Main {
+    public static void main (String [] args) {
+        Book [] book = new Book[5];
+        book[0]  = new Book(1, "АСТ 2062:время машин. ", "Уолш Т. ", "АСТ ", 2019, 320, 29.30, "Твердый");
+        book[1] = new Book(2,"Homo Futurus. Облачный Мир. ", "Закарайя А. ", "АСТ ", 2018, 540, 18.33, "Твердый" );
+        book[2] = new Book(3, "Разговор с вождем. ", "Роман Злотников ", "Издательство Э ", 2015, 416, 10.20, "Мягкий");
+        book[3] = new Book(4, "Путь к Порогу. ", "Роман Злотников ", "Сегмент ", 2011, 354, 12.54, "Мягкий");
+        book[4] = new Book(5, "Сила подсознания, или Как изменить жизнь за 4 недели. ", "Диспенза Джо ", "Эксмо", 2013, 18, 20.20, "Твердый");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(book[i].toString());
+        }
+        System.out.println("\n  Книги выпущенные после 2015 года. \n");
+        for (int i = 0; i < 5; i++) {
+            book[i].showYear();
+        }
+        System.out.println("\n  Книги выпущенные издательством АСТ \n");
+        for (int i = 0; i < 5; i++) {
+            book[i].showPublishing();
+        }
+        System.out.println("\n  Книги Романа Злотникова \n");
+        for (int i = 0; i < 5; i++) {
+            book[i].getAuthor();
+
+        }
+
+    }
+}
